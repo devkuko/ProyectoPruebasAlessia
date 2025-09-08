21db1d168e844333f1fb5aeae5fa48d510fd9b48
KukoDev <devkuko@gmail.com>
2025-09-08 17:53:57 +0200

    añadimos lo que hicimos en la clase 1

---
+++ Clase1/Coche.cs
@@ -0,0 +1,39 @@
+﻿
+public class Coche
+{
+    private int id;
+    public int velocidadMaxima;
+    public const double PI = 3.14;
+
+    public Coche(int id, int v2) //constructor 1
+    {
+        this.id = id;
+        this.velocidadMaxima = v2;
+    }
+
+    public Coche(int id) //constructor 2
+    {
+        this.id = id;
+    }
+
+    public Coche() //constructor 3
+    {
+        this.velocidadMaxima = 120;
+    }
+
+
+    public int GetVelocidadmaximaPermitida()
+    {
+        return this.velocidadMaxima + 20;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public int getId()
+    {
+        return this.id;
+    }
+
+}
---
+++ Clase1/Program.cs
@@ -0,0 +1,48 @@
+﻿Coche coche1 = new Coche(1, 120);
+
+//coche1.id = 100;
+Console.WriteLine(coche1.getId());
+Console.WriteLine(coche1.ToString());
+
+coche1.setId(100);
+
+Console.WriteLine(coche1.getId());
+
+modificarCoche(coche1);
+
+Console.WriteLine(coche1.getId());
+
+void modificarCoche(Coche c1)
+{
+    c1.setId(500);
+}
+
+int[] numeros = { 1, 2, 3 };
+Console.WriteLine("Antes de CambiarArray: " + string.Join(", ", numeros));
+CambiarArray(new int[] { 1, 2, 3 });
+Console.WriteLine("Después de CambiarArray: " + string.Join(", ", numeros));
+
+
+void CambiarArray(int[] numeros) // paso por referencia
+{
+    numeros[0] = 999; // modifica el array original
+
+}
+
+
+
+int numero = 5;
+Console.WriteLine("Antes de CambiarValor: " + numero);
+CambiarValor(numero);
+Console.WriteLine("Después de CambiarValor: " + numero);
+
+void CambiarValor(int numero)  //paso por valor
+{
+    numero = 999; // solo se cambia la copia
+}
+
+//Coche coche2 = coche1; // ref  posicion de memoria
+Coche coche2 = new Coche(2, 120);
+
+Coche coche3 = new Coche();
+coche3.setId(3);
